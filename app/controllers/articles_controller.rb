09ab2679dc29dc0f4b40569a12de109b398aebf5
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-12 16:51:58 +1100

    "it is soooo easy with resources :model" vincent

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# class
+class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show edit update destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create
+    redirect_to articles_path
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
